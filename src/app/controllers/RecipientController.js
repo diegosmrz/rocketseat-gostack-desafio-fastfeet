474b9a431272bf67cbe74cd36396859cd2d64e22
diegomrz <diegomrz@outlook.com>
2020-02-12 03:51:25 -0300

    #014 Validação de dados de entrada - yup [ recipient, session e user]

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      address: Yup.string().required(),
+      number: Yup.integer().required(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Scham validation invalid' });
+    }
+
     const recipientExists = await Recipient.findOne({
       where: {
         name: req.body.name,
@@ -31,6 +45,18 @@ class RecipientController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      address: Yup.string(),
+      number: Yup.integer(),
+      city: Yup.string(),
+      state: Yup.string(),
+      cep: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Scham validation invalid' });
+    }
     console.log(req.userID);
 
     return res.json(req.userID);
