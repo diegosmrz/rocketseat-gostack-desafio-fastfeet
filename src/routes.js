23194b8a6f0076d3d2a8fb2f2221993f4855be82
diegomrz <diegomrz@outlook.com>
2020-02-12 02:59:19 -0300

    #011 Sessão e Validação de email/senha - sessioncontroller e jsonwebtoken

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import RecipientController from './app/controllers/RecipientController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/recipients', RecipientController.store);
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
